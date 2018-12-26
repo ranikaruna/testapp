cfbd5921cf87acbfe0a2ec744e9a38d4010d93f8
chris <44230073+global-ecommerce@users.noreply.github.com>
2018-12-26 15:47:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -10,8 +10,8 @@ c = Counter('requests', 'Number of requests served, by http code', ['http_code']
 
 @app.route('/')
 def hello():
-    return handle_request_html()
-#san
+    return new_handle_request_html()
+
 # http request handler for all
 def handle_request_html():
     os.environ['SUCCESS_RATE'] = "90"
